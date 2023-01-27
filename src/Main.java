58e5d38dffce78c61f05c93a5f6327a153849fae
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-01-27 12:49:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 3587;
+        System.out.println("Кол-во бонусов " + x/100);
+        System.out.println("Сумма с учетом бонусов " + (x + x/100));
+    }
+}
